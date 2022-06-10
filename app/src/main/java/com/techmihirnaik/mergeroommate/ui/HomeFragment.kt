15a77dfc1da358cf0367e281df8d1eb989ae5df7
masudkabir <masudkabir965@gmail.com>
2022-06-11 00:39:51 +0530

    schedule page

--- app/src/main/java/com/techmihirnaik/mergeroommate/ui/HomeFragment.kt
+++ app/src/main/java/com/techmihirnaik/mergeroommate/ui/HomeFragment.kt
@@ -1,23 +1,33 @@
 package com.techmihirnaik.mergeroommate.ui
 
-import android.graphics.Color
-import android.graphics.Color.TRANSPARENT
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.techmihirnaik.mergeroommate.R
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.techmihirnaik.mergeroommate.databinding.FragmentHomeBinding
+import com.techmihirnaik.mergeroommate.schedule.Schedule
+
 
 class HomeFragment : Fragment() {
-
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+         binding = DataBindingUtil.inflate(
+            inflater, com.techmihirnaik.mergeroommate.R.layout.fragment_home , container, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+         binding.hotels.setOnClickListener {
+            val intent= Intent(context, Schedule::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
     }
 
     companion object {
